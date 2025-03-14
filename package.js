5c600c68db63efe15ae4f021dd2593e1b99cbfc9
NARUTO <senguptakoulik94@gmail.com>
2025-03-14 13:50:47 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "Star-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^7.0.0",
+    "express": "^4.18.2",
+    "fs": "^0.0.1-security"
+  }
+}
